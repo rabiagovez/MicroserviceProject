2a3b0747ee67e03cf6955ee8aec9d958a5f7d44f
rabiagovez <116706039+rabiagovez@users.noreply.github.com>
2025-05-19 17:37:37 +0300

    Controller Deneme

---
+++ ECommerCase.UI/Controllers/DenemeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerCase.UI.Controllers
+{
+    public class DenemeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
